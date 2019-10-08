ab3db60197e5d69bb2aca379e610630ca0283276
Tadas <t..petruskevicius@gmail.com>
2019-10-08 14:50:06 +0300

    Foreach pradzia

---
+++ app/php/php.php
@@ -0,0 +1,26 @@
+<?php
+
+$auto = [
+    "Audi" => [
+        [
+            "model" => "A1",
+            "fuel" => "Petrol",
+        ],
+        [
+            "model" => "Q3",
+            "fuel" => "Diesel",
+        ],
+    ],
+    "BMW" => [
+        [
+            "model" => "530",
+            "fuel" => "Petrol",
+        ],
+        [
+            "model" => "420",
+            "fuel" => "Diesel",
+        ],
+    ],
+];
+
+?>
---
+++ app/views/first.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!--bootstrap-->
+    <link rel="stylesheet" href="../../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../../assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="../../assets/css/bootstrap-grid.css">
+
+    <link rel="stylesheet" href="../../assets/css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="../../index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="first.php">First</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="second.php">Second</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<p>Cia yra pagrindinis first.php</p>
+<!--bootstrap js-->
+<script src="../../assets/js/jqery.js"></script>
+<script src="../../assets/js/bootstrap.js"></script>
+<script src="../../assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
